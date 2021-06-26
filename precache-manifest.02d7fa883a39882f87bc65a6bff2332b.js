9536fe89c656bd0b3f43c489080f331365a4e129
Angela-inca <y1gu@ucsd.edu>
2021-06-26 19:43:39 +0800

    Updates

---
+++ precache-manifest.02d7fa883a39882f87bc65a6bff2332b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8c801e8fcd3728ea4595205f07e0fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a796929255d74a0b0f8",
+    "url": "/static/css/2.309bc4dc.chunk.css"
+  },
+  {
+    "revision": "dbd65dce3426d659b9fb",
+    "url": "/static/css/main.3c487063.chunk.css"
+  },
+  {
+    "revision": "9a796929255d74a0b0f8",
+    "url": "/static/js/2.540ef37d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.540ef37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd65dce3426d659b9fb",
+    "url": "/static/js/main.b0b69919.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "591be10972762903f9936fad58a0a9fa",
+    "url": "/static/media/Angela.591be109.JPG"
+  },
+  {
+    "revision": "05cd336e1ef4554ac21e6c76fed466dd",
+    "url": "/static/media/circle.05cd336e.png"
+  },
+  {
+    "revision": "cd8e532bdd9558ca034829be4a555029",
+    "url": "/static/media/geisel.cd8e532b.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "ef87f5ba3f44676cf7efedd56101c880",
+    "url": "/static/media/ibm.ef87f5ba.png"
+  },
+  {
+    "revision": "cb37593b3329b7d5a0431c0ed08e7e75",
+    "url": "/static/media/launchit1.cb37593b.png"
+  },
+  {
+    "revision": "e1173bc182ba9783ea6448d183d94613",
+    "url": "/static/media/launchit2.e1173bc1.png"
+  },
+  {
+    "revision": "d79adac48347c44a32e7373f967ca9fc",
+    "url": "/static/media/phonological.d79adac4.png"
+  },
+  {
+    "revision": "15a4296e823e194f935d0c4a66c270af",
+    "url": "/static/media/thehack.15a4296e.jpeg"
+  },
+  {
+    "revision": "f79f462332da597a47986e0df953a736",
+    "url": "/static/media/thumb.f79f4623.png"
+  },
+  {
+    "revision": "fd93755236328d80d05a7527434145b6",
+    "url": "/static/media/ucsd.fd937552.jpeg"
+  },
+  {
+    "revision": "513c299c008461f6b8f63a15dc291659",
+    "url": "/static/media/xplore1.513c299c.png"
+  },
+  {
+    "revision": "fb838fbdd9b9268f3555628ad23282d0",
+    "url": "/static/media/xplore2.fb838fbd.png"
+  }
+]);
